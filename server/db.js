ec74ec7ddf864dc1c990b3ea16bc504e6721e90d
VISMAYA SOLANKI <vismayasolanki@192.168.1.5>
2023-05-18 03:29:42 +0530

    copde

--- server/db.js
+++ server/db.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 
 function connect() {
   return new Promise((resolve, reject) => {
-    mongoose.connect('mongodb://127.0.0.1:27017/foodApp', {
+    // mongoose.connect('mongodb://127.0.0.1:27017/foodApp', {
+    mongoose.connect('mongodb://mongo/foodApp', {
+
       useNewUrlParser: true,
       useUnifiedTopology: true,
 
